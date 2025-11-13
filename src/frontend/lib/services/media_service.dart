9c49629d5d3b14ee7f1a0c183fa3fa0ec0bf5de9
주현수 <z_hys@juhyeonsuui-MacBookAir.local>
2025-11-13 21:32:31 +0900

    fix: reverse conditional import to load web service by default for webOS

--- src/frontend/lib/services/media_service.dart
+++ src/frontend/lib/services/media_service.dart
@@ -1,6 +1,6 @@
 import 'media_service_interface.dart';
-import 'media_service_stub.dart'
-    if (dart.library.js) 'media_service_web.dart';
+import 'media_service_web.dart'
+    if (dart.library.io) 'media_service_stub.dart';
 
 export 'media_service_interface.dart';
 
